a265a0597bbeb492fbaf60d76608dcffa1b81429
Jordan Johnston <jordanjohnston97@outlook.com>
2021-05-16 17:58:01 +0100

    - test skeleton

--- src/components/MainPanel/index.js
+++ src/components/MainPanel/index.js
@@ -1,7 +1,9 @@
 import { Button } from '@chakra-ui/button';
+import { Grid, GridItem } from '@chakra-ui/layout';
 import { StackDivider } from '@chakra-ui/layout';
 import { VStack } from '@chakra-ui/layout';
 import { Box, Text } from '@chakra-ui/layout';
+import { Skeleton } from '@chakra-ui/skeleton';
 import React, { useState, useEffect, useRef } from 'react';
 import * as requests from '../../helpers/request';
 import ItemList from '../ItemList';
@@ -14,7 +16,7 @@ const MainPanel = () => {
 	const [library, setLibrary] = useState({});
 	const itemsIncrement = 20;
 	const [numItems, setNumItems] = useState(itemsIncrement);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const getLibraryFile = () => {
 		setIsLoading(true);
@@ -80,32 +82,60 @@ const MainPanel = () => {
 	};
 
 	return (
-		<Box overflow={'auto'} maxH={'100vh'}>
-			<VStack
-				align={'stretch'}
-				divider={<StackDivider borderColor={'gray.200'}/>}
-				spacing={4}
-			>
-				<Box>
-					<Text hidden={!isLoading} style={{padding: 8, margin: 16}}>-- Api Request in Progress -- </Text>
-					<Button onClick={buildLibrary}>Build Library</Button>
-					<Button onClick={getLibraryFile}>Get Library File</Button>
+		<>
+			{isLoading &&
+				<Grid
+					gap={6}
+					maxH={'80vh'}
+					overflowY={'auto'}
+					templateColumns={'repeat(3, 1fr)'}
+				>
+					{[1,2,3,4,5].map(i => {
+						return (
+							<React.Fragment key={`skeleton_loading_${i}`}>
+								<GridItem>
+									<Skeleton/>
+								</GridItem>
+								<GridItem>
+									<Skeleton/>
+								</GridItem>
+								<GridItem>
+									<Skeleton/>
+								</GridItem>
+							</React.Fragment>
+						);
+					})}
+				</Grid>
+			}
+			{!isLoading &&
+				<Box overflow={'auto'} maxH={'100vh'}>
+					<VStack
+						align={'stretch'}
+						divider={<StackDivider borderColor={'gray.200'}/>}
+						spacing={4}
+					>
+						<Box>
+							<Text hidden={!isLoading} padding={'2'} margin={'4'}>-- Api Request in Progress -- </Text>
+							<Button onClick={buildLibrary} margin={'4'}>Build Library</Button>
+							<Button onClick={getLibraryFile} margin={'4'} marginLeft={'0'}>Get Library File</Button>
+						</Box>
+						<ItemList
+							displayKeys={['Album', 'Artist', 'FileType', 'Title', 'Year']}
+							infiniteScroll
+							items={Object.keys(library).slice(0, numItems).map(k => library[k])}
+							itemKey={'Key'}
+							listType={'shitty'}
+							onClickItem={onClickLibraryItem}
+							onInfiniteScrollBottom={onInfiniteScrollBottom}
+						/>
+						<Player
+						currentlyPlaying={currentlyPlaying}
+						source={audioSrc}
+						/>
+					</VStack>
 				</Box>
-				<ItemList
-					displayKeys={['Album', 'Artist', 'FileType', 'Title', 'Year']}
-					infiniteScroll
-					items={Object.keys(library).slice(0, numItems).map(k => library[k])}
-					itemKey={'Key'}
-					listType={'shitty'}
-					onClickItem={onClickLibraryItem}
-					onInfiniteScrollBottom={onInfiniteScrollBottom}
-				/>
-				<Player
-				currentlyPlaying={currentlyPlaying}
-				source={audioSrc}
-				/>
-			</VStack>
-		</Box>
+			}
+		</>
 	);
 };
 
