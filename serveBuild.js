ad21e2494e2ac3abc009216b429d0a01cb797ac9
jordanjohnston <jordanjohnston97@outlook.com>
2021-10-29 15:42:02 +0100

    made serveBuild.js a bit less shit

--- serveBuild.js
+++ serveBuild.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { networkInterfaces } = require('os');
+
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+const interfaces = networkInterfaces();
+// todo: make this not shit, and actually find the first "active" one
+const defaultInterface = interfaces['en0'].filter(eth => eth.family === 'IPv4')[0];
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-console.log(`${new Date()} serving content to http://localhost:${port}`);
-app.listen(port);
+console.log(`${new Date()} serving content to:`);
+app.listen(port, () => {
+  console.log(`  -> http://localhost:${port}\t(locally)`);
+});
+app.listen(port, defaultInterface.address, () => {
+  console.log(`  -> http://${defaultInterface.address}:${port}\t(on local network)`);
+});
